e251cde93a5d9af6e5c7d8fad91278bad2f5e63e
JeongWonLim <cdnljw@gmail.com>
2022-03-30 20:51:32 +0900

    코드연결

--- DBschool/App/urls.py
+++ DBschool/App/urls.py
@@ -3,6 +3,15 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    #정원
     path('join', views.join),
+
+    #건호
     path('', views.home),
+
+    #준엽
+    path('login', views.signup),
+
+    #부건
+
 ]
--- DBschool/App/views.py
+++ DBschool/App/views.py
@@ -1,7 +1,40 @@
 from django.shortcuts import render
+from django.urls import reverse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+#정원
 def join(request):
     return render(request,"App/join.html")
 
+
+#건호
 def home(request):
     return render(request, "App/home.html")
+
+
+
+#준엽
+def signup(request):
+    if request.method == "GET":
+        return render(request, 'App/signup.html')
+
+
+    elif request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        print(request.POST)
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            print("인증 성공")
+            return HttpResponseRedirect(reverse('posts:index'))
+        else:
+            print("인증 실패")
+            return render(request, 'App/signup.html')
+    return render(request, 'App/signup.html')
+
+
+#부건
